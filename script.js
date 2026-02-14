ef2e124b29f065faaecb190b0f2f454923aec045
emir0315 <emir.guo0315@gmail.com>
2026-02-14 13:35:30 +0800

    2

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
-let likeCount = 0;
-
-function showMore() {
-    const info = document.getElementById("moreInfo");
-    info.classList.toggle("hidden");
+function showMore(btn) {
+    const card = btn.parentElement;
+    const more = card.querySelector(".more");
+    more.classList.toggle("hidden");
 }
 
-function likeGame() {
-    likeCount++;
-    document.getElementById("likeCount").innerText = likeCount;
+function likeGame(btn) {
+    const span = btn.querySelector("span");
+    let count = parseInt(span.innerText);
+    count++;
+    span.innerText = count;
 }
 
 function toggleDarkMode() {
     document.body.classList.toggle("dark-mode");
+}
+
+function toggleMusic() {
+    const music = document.getElementById("bgMusic");
+    if (music.paused) {
+        music.play();
+    } else {
+        music.pause();
+    }
 }
